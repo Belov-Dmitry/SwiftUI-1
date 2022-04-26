68d34c3e676fb57b6cf22f0aa7fd32903b6a9521
BelovDmitry <1@belov.es>
2022-04-26 22:18:37 +0300

    Lesson-1
    Реализуйте экран логина для вашего приложения VK:
    Вёрстка экрана логина

---
+++ SwiftUI-1/SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_1App.swift
+//  SwiftUI-1
+//
+//  Created by Dmitry Belov on 26.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
